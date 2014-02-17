85d04c581ebea5ec5c4c29cb81815fa8c67eabf4
Rodrigo Figueroa <bartoindahouse@bartoindahouse.iptv\.microsoft\.com>
2014-02-17 18:02:10 -0300

    login working for facebook as well as prior twitter

--- Documents/code/tnc/national_1/config/initializers/omniauth.rb
+++ Documents/code/tnc/national_1/config/initializers/omniauth.rb
@@ -3,4 +3,5 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   # provider :twitter, ENV['KzA2CfEZewV6vvoKUSSjYg'],ENV['nRE4LT49NLygA1yObzSQmhrGpBKTlMjeDNxh0cQo']
     provider :developer unless Rails.env.production?
     provider :twitter, 'KzA2CfEZewV6vvoKUSSjYg', 'nRE4LT49NLygA1yObzSQmhrGpBKTlMjeDNxh0cQo'
+    provider :facebook, '676771872383735', '1c723973bb3824edb6ad3507d947676c'
 end
