bebe9037e4c920386568e794e396dc9385fe3947
Rodrigo Figueroa <bartoindahouse@bartoindahouse.iptv\.microsoft\.com>
2014-02-17 14:28:15 -0300

    login ahora mostrando nombre usuario y sacando boton de login

--- Documents/code/tnc/national_1/app/controllers/sessions_controller.rb
+++ Documents/code/tnc/national_1/app/controllers/sessions_controller.rb
@@ -1,4 +1,16 @@
 class SessionsController < ApplicationController
+  # env['omniauth.auth'] we have this omniauth hash
+
+  def create
+    auth = env['omniauth.auth']
+    user = User.find_by_provider_and_uid(auth['provider'], auth['uid']) || User.create_with_omniauth(auth)
+
+    #back to controller now that we have a user we can sign him in by storing his user id and his session
+    session[:user_id] = user.id
+    redirect_to :inicio_index , notice: 'signed in'
+  end
+
+
   # def create
   #   @user = User.find_or_create_from_auth_hash(auth_hash)
   #   self.current_user = @user
